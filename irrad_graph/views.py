871ebdc030cea1062b7271c9be016c679ba65297
Chris Kollias <chriskollias500@gmail.com>
2022-06-15 15:14:37 -0700

    IrradGraphInputs model renamed to IrradGraph, imports updated to reflect change

--- irrad_graph/views.py
+++ irrad_graph/views.py
@@ -1,43 +1,24 @@
 from django.shortcuts import render
 from .forms import LatLongForm
-from .models import IrradGraphInputs
-import matplotlib.pyplot as plt
+from .models import IrradGraph
 
-from API.solar_api import NREL_API
+from API.solar_api import clean_raw_df, load_raw_df, calc_monthly_averages, display_csv_graph
 
 
-# Create your views here.
 def main_view(request, *args, **kwargs):
     APP_MEDIA_PATH = 'irrad_graph/'
-    APP_SAVE_LOCATION = 'media/irrad_graph/'
 
+    raw_df = load_raw_df('solar_info20200228184632.csv')
+    metadata, df = clean_raw_df(raw_df)
+    monthly_averages = calc_monthly_averages(df)
+    graph_image = display_csv_graph(monthly_averages)
+    image_path = APP_MEDIA_PATH + graph_image
     form = LatLongForm()
-    obj = None
-
-    if request.method == 'POST':
-        form = LatLongForm(request.POST)
-        if form.is_valid():
-            latitude = form.cleaned_data['latitude']
-            longitude = form.cleaned_data['longitude']
-            year = form.cleaned_data['year']
-            #attrs = 'ghi,dhi,dni,wind_speed,air_temperature,solar_zenith_angle'
-            attrs = 'ghi,dhi,dni'
-            print(f'lat:{latitude} long:{longitude} year:{year} attrs:{attrs}')
-
-            api = NREL_API()
-            graph_image = api.get_monthly_averages(latitude, longitude, year, attrs)
-            image_path = APP_MEDIA_PATH + graph_image
-            APP_SAVE_LOCATION += graph_image
-            plt.savefig(APP_SAVE_LOCATION, dpi=300)
-            form.cleaned_data['image'] = image_path
-            obj = IrradGraphInputs.objects.create(**form.cleaned_data)
-
-            form = LatLongForm()
-        else:
-            print(form.errors)
-
+    obj = IrradGraph.objects.create(lat=33.21, long=-97.14, year=2009, image=image_path)
     context = {
         'form': form,
         'obj': obj,
+        'metadata': metadata,
     }
+
     return render(request, 'irrad_graph/irrad_graph_page.html', context)
