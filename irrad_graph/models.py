2b585a8c7c52e8b7d46cde5ca49270085f3f80e4
Chris Kollias <chriskollias500@gmail.com>
2022-06-15 14:23:44 -0700

    updated field names in IrradGraphInputs to match official abbreviations

--- irrad_graph/models.py
+++ irrad_graph/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 
-# Create your models here.
 class IrradGraphInputs(models.Model):
-    latitude = models.DecimalField(max_digits=7, decimal_places=4)
-    longitude = models.DecimalField(max_digits=7, decimal_places=4)
+    lat = models.DecimalField(max_digits=7, decimal_places=4)
+    long = models.DecimalField(max_digits=7, decimal_places=4)
     year = models.IntegerField()
     image = models.ImageField(default='irrad_graph/empty.png', upload_to='irrad_graph')
